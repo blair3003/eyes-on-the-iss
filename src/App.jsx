6e586575046149dfbea41e295bd7bcb8f2110dfa
Blair Fleming <blair3003.bf@gmail.com>
2023-02-20 11:18:58 +0000

    location default

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     return (
         <div className="App">
             <h1>Eyes on the ISS</h1>
-            <p>Location: {JSON.stringify(location.lat)}, {JSON.stringify(location.lon)}</p>
+            <p>Location: {JSON.stringify(location?.lat)}, {JSON.stringify(location?.lon)}</p>
 
         </div>
     )
--- src/store.jsx
+++ src/store.jsx
@@ -2,13 +2,17 @@ import { useState, useEffect } from 'react'
 
 export const useApplicationState = () => {
 
-	const [location, setLocation] = useState(null)
+	const [location, setLocation] = useState({
+		lat: 0,
+		lon: 0
+	})
 
 	useEffect(() => {
 		const localLocation = localStorage.getItem('location')
 		if (localLocation) {
 			console.log('Getting location from local storage')
 			setLocation(JSON.parse(localLocation))
+			console.log('Location set')
 		} else {
 			console.log("Getting location from user's device")
 			navigator.geolocation.getCurrentPosition(
